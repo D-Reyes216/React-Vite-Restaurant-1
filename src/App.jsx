2246b796bd56f64f5279ec86982ed8332cf11fdc
D-Reyes216 <davepr216@gmail.com>
2024-01-31 13:54:22 -0800

    added jsx files for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import Header from 'Header.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
+import Header from './components/Header/Header.jsx';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar />
+      <Header />
+    </div>
   )
 }
 
