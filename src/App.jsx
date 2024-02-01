2a52be04a2fdd75f98c1b866c0ca2dfce9695b6d
D-Reyes216 <davepr216@gmail.com>
2024-02-01 03:10:00 -0800

    added image assets, completed navbar/header v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx';
 import Header from './components/Header/Header.jsx';
+import Menu from './components/Menu/Menu.jsx';
 
+import './App.css';
 const App = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <Header />
-    </div>
+      <Menu />
+    </>
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {GiHamburgerMenu} from 'react-icons/gi';
 
+import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div class="app__navbar">
+      <img class="app__navbar-logo" src="src/assets/cc_logo.png" alt="cc-logo" />
+      <img class="app__navbar-title" src="src/assets/cc-title.png" alt="cc-title" />
+      <ul class="app__navbar-links">
+        <li>Home</li>
+        <li>Menu</li>
+        <li>Gallery</li>
+        <li>Contact</li>
+      </ul>
+      <div class="app__navbar-dropdown">
+        <GiHamburgerMenu onClick={() =>{}}/>
+      </div>
+    </div>
+
   )
 }
 
